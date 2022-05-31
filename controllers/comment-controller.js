c2d77de5633e9629c1ae0de38f910074c1b33d37
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-31 09:33:50 -0400

    add comment-controller

---
+++ controllers/comment-controller.js
@@ -0,0 +1,15 @@
+const { Comment, PIzza } = require('../models');
+
+const commentController = {
+    // add commetn to pizza
+    addComment() {
+
+    },
+
+    // remove comment
+    removeComment() {
+
+    }
+};
+
+module.exports = commentController;
