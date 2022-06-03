a1108af052f6c40eae65a0c229c620f67e19af73
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-02 22:28:04 -0400

    add upload pizza function

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -28,7 +28,7 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-// this funciton will be executed if we attempt to submit a new pizza and there's no internet connection 
+// this function will be executed if we attempt to submit a new pizza and there's no internet connection 
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_pizza'], 'readwrite');
@@ -38,4 +38,17 @@ function saveRecord(record) {
 
     // add record to your store with add method 
     pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access your object store
+    const pizzaObjectStore = transaction.pizzaObjectStore('new_pizza');
+
+    // get all record from store and set to a variable 
+    const getAll = pizzaObjectStore.getAll();
+
+    // more to come... 
 }
