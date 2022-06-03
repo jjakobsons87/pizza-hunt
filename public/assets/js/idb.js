9c7f7d50918a18f50b1394f4eb1f0d214263f26b
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-02 22:17:01 -0400

    add saverecord functino to idb.js

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -3,7 +3,7 @@ let db;
 // establish a connection to IndexedDB database called 'pizza-hunt' and set it to version 1 
 const request = indexedDB.open('pizza_hunt', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// this event will emit if the database version changes (nonexistent to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
@@ -13,12 +13,12 @@ request.onupgradeneeded = function(event) {
 
 // upon a successful 
 request.onsuccess = function(event) {
-    // when db is successfully created with its ovject store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable 
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable 
     db = event.target.result;
 
     // check if app is online, if yes run uploadPizza() function to send all local db data to api 
     if (navigator.onLine) {
-        // we havent create this yet but will soon so commented out for now 
+        // we haven't create this yet but will soon so commented out for now 
         // uploadPizza(); 
     }
 };
@@ -27,3 +27,15 @@ request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// this funciton will be executed if we attempt to submit a new pizza and there's no internet connection 
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the objec store for 'new_pizza'
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store with add method 
+    pizzaObjectStore.add(record);
+}
